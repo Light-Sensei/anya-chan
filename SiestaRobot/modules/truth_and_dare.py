0962d9394aad10a53d5b4955d3f519b7639c1b46
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-04-18 21:36:45 +0530

    Update truth_and_dare.py

--- SiestaRobot/modules/truth_and_dare.py
+++ SiestaRobot/modules/truth_and_dare.py
@@ -16,11 +16,11 @@ def truth(update: Update, context: CallbackContext):
 def dare(update: Update, context: CallbackContext):
     context.args
     update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
-def sigma(update: Update, context: CallbackContext):
-    update.effective_message.reply_video(random.choice(truth_and_dare_string.SIGMA))
 
-def cosplay(update: Update, context: CallbackContext):
-    update.effective_message.reply_photo(random.choice(truth_and_dare_string.COSPLAY))
+
+
+
+
 
 
 
@@ -28,8 +28,7 @@ def cosplay(update: Update, context: CallbackContext):
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
-SIGMA_HANDLER = DisableAbleCommandHandler("sigma", sigma, run_async=True)
-COSPLAY_HANDLER = DisableAbleCommandHandler("cosplay", cosplay, run_async=True)
+
 
 
 
@@ -37,6 +36,5 @@ COSPLAY_HANDLER = DisableAbleCommandHandler("cosplay", cosplay, run_async=True)
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
-dispatcher.add_handler(SIGMA_HANDLER)
-dispatcher.add_handler(COSPLAY_HANDLER)
+
 
