599c7264e86ce258f14569cd5a87636b58df4c1d
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-04-27 12:26:24 +0530

    Update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -9,13 +9,13 @@ from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/10deb009aa778c8be099a.jpg"
+PHOTO = "https://telegra.ph/file/daae9edfff5f40077f11d.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Yo!! [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Akame.** \n\n"
   TEXT += "♡ **I'm Working Properly Baka!!** \n\n"
-  TEXT += f"♡ **My Darling : [Devoid Namikaze](https://t.me/DEVOID_10)** \n\n"
+  TEXT += f"♡ **My Loid-San : [Gautam](https://t.me/follow_me_kids)** \n\n"
   TEXT += f"♡ **Library Version :** `{telever}` \n\n"
   TEXT += f"♡ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"♡ **Pyrogram Version :** `{pyrover}` \n\n"
